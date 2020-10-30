2bc8d66b17ee138c2eac93186a8f53e2217dcbae
Lonti6 <72629247+Lonti6@users.noreply.github.com>
2020-10-31 02:42:29 +0500

    Add Generate Ratings
    Add Generate Ratings

---
+++ Assets/Scripts/GenerateRectangles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateRectangles : MonoBehaviour
+{
+    public Transform prefabOne;
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(prefabOne, transform);
+
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
