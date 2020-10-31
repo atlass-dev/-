cb98de8dbce713d4b6f28804feb277f22c85be18
bdm <bdm1999@yandex.ru>
2020-10-31 20:58:57 +0500

    bad commit

--- Assets/Scripts/Chat.cs
+++ Assets/Scripts/Chat.cs
@@ -9,21 +9,65 @@ using Image = UnityEngine.UI.Image;
 
 public class Chat : MonoBehaviour
 {
+    public static string MyName = "Борис";
     public string[] arraySmall;
     public Sprite[] spr;
     public GameObject pref;
     private List<GameObject> list = new List<GameObject>();
+    public Dictionary<string, List<string[]> > dic = new Dictionary<string, List<string[]> >();
+    public GameObject prefChat;
+    public Button send;
+    public Text textview;
+    public GameObject movied;
+    public Vector2 start;
+    public Vector2 end;
+    
     void Start()
     {
-        for (var e = 0; e < arraySmall.Length; e++) 
+        ChooseDialog();
+    }
+
+    private void ChooseDialog()
+    {
+        RemovedList();
+        for (var e = 0; e < arraySmall.Length; e++)
         {
             var a = Instantiate(pref, transform);
-            var fontS = a.GetComponentInChildren<Text>().fontSize;
-            var b = a.GetComponentsInChildren<Image>()[0].rectTransform.sizeDelta.y;
-            b = fontS * 2;
+
             a.GetComponentInChildren<Text>().text = arraySmall[e];
             a.GetComponentsInChildren<Image>()[1].sprite = spr[e];
+            var e1 = e;
+            a.GetComponent<Button>().onClick.AddListener(() => OpenChat(arraySmall[e1]));
+            list.Add(a);
         }
+    }
+
+    void OpenChat(string name)
+    {
+        //var an = movied.AddComponent<AnimationMoveTo>();
+       
+        RemovedList();
+        if (!dic.ContainsKey(name))
+            dic[name] = new List<string[]>();
+        List<string[]> arr = dic[name];
+        foreach (var ss in arr)
+        {
+         
+            var go = Instantiate(prefChat, transform);
+            var array = go.GetComponentsInChildren<Text>();
+            array[0].text = ss[0];
+            array[1].text = ss[1];
+            list.Add(go);
+        }
+        
+        send.onClick.RemoveAllListeners();
+        send.onClick.AddListener(() => AddMessage(name));
+    }
+
+    void AddMessage(string chatName)
+    {
+        dic[chatName].Add(new []{MyName, textview.text});
+        OpenChat(chatName);
     }
     private void RemovedList()
     {
