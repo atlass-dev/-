149accc0da6aa656277d6a5d130151666d6adc2a
bdm <bdm1999@yandex.ru>
2020-11-01 01:43:27 +0500

    raiting

--- Assets/Scripts/GenerateRectangles.cs
+++ Assets/Scripts/GenerateRectangles.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,11 +16,12 @@ public class GenerateRectangles : MonoBehaviour
         {
             var j = Instantiate(prefabOne, transform);
             var comps = j.GetComponentsInChildren<Image>();
+            var arr = comps.Where(img => img.CompareTag("Icons")).ToArray();
             var compsText = j.GetComponentsInChildren<Text>();
             for (var e = 4; e < 9; e++)
             {
-                compsText[e - 4].text = "Иванов Иван\n"+texts[index]; 
-                comps[e].sprite = sprit[index++];
+                compsText[e - 4].text = texts[index]  +"\n" + (index + 13 - 2 * e) + " место"; 
+                arr[e - 4].sprite = sprit[index++];
             }
         }
     }
