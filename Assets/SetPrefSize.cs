71115c71cb10442a5cf430b12a05f61913709cf3
bdm <bdm1999@yandex.ru>
2020-10-31 19:17:10 +0500

    bad commit

---
+++ Assets/SetPrefSize.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetPrefSize : MonoBehaviour
+{
+    private RectTransform text;
+    public float width = 10;
+    public float height = 10;
+    void Start()
+    {
+        text = GetComponentInChildren<Text>().GetComponent<RectTransform>();
+        var my = GetComponent<RectTransform>();
+        print(my.rect);
+        GetComponentInChildren<Text>().GetComponent<ContentSizeFitter>().verticalFit =
+            ContentSizeFitter.FitMode.PreferredSize;
+        print(text.rect);
+        my.sizeDelta = new Vector2(text.rect.width + width, text.rect.height + height);
+    }
+    
+}
